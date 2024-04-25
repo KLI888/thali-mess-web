27634493ca541b62fa5c97e513a44c1c79fcc37f
Your Name <krushnaborase40@gmail.com>
2024-04-25 14:16:21 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,58 @@ import Header from './components/header/Header'
 import {assets} from './assets/assets'
 import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './components/about/About'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Header />
+          <Home />
+          <Footer />
+        </>
+      )
+    },
+    {
+      path: '/about',
+      element: (
+        <>
+          <Header />
+          <About />
+          <Footer />
+        </>
+      )
+    },
+    {
+      path: '/locatemess',
+      element: (
+        <>
+          <Header />
+          <About />
+          <Footer />
+        </>
+      )
+    },
+    {
+      path: '/addmess',
+      element: (
+        <>
+          <Header />
+          <About />
+          <Footer />
+        </>
+      )
+    }
+  ]);
+
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <RouterProvider router={router} />
+
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import {assets} from '../../assets/assets'
-
-import './header.css'
+import React, { useState } from 'react';
+import { assets } from '../../assets/assets';
+import './header.css';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
-
     const [menuOn, setMenuOn] = useState(false);
+    const [menu, setMenu] = useState("home");
+
     return (
         <header>
             <div className="navbar container">
                 <div className="logo">
-                    <a href="">
+                    <Link to="/">
                         <img src={assets.logo} alt="" />
-                    </a>
+                    </Link>
                 </div>
                 <div className="navbar_right">
                     <div className="navbar_links">
                         <div className="navar_ul">
-                        <ul className={menuOn==true ? 'active' : ''}>
-                            <li><a href="" className="active">Home</a></li>
-                            <li><a href="">Locatemess</a></li>
-                            <li><a href="">Addmess</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="/" className="navbar_btn">Sign In</a></li>
-
-                            <i onClick={()=>{setMenuOn(false)}} class='bx bx-chevron-right'></i>
-                        </ul>
+                            <ul className={menuOn === true ? 'active' : ''}>
+                                <li><NavLink to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</NavLink></li>
+                                <li><NavLink onClick={() => setMenu("locate")} className={menu === "locate" ? "active" : ""} to="/locatemess">Locatemess</NavLink></li>
+                                <li><NavLink onClick={() => setMenu("addmess")} className={menu === "addmess" ? "active" : ""} to="/addmess">Addmess</NavLink></li>
+                                <li><NavLink onClick={() => setMenu("about")} className={menu === "about" ? "active" : ""} to="/about">About</NavLink></li>
+                                <li><NavLink className="navbar_btn" to="/">Sign In</NavLink></li>
+                                <li><i onClick={() => { setMenuOn(false) }} className='bx bx-chevron-right'></i></li>
+                            </ul>
                         </div>
                     </div>
                     <div className="toggle_menu">
-                        <i onClick={()=>{setMenuOn(true)}} class='bx bx-chevron-left'></i>
+                        <p onClick={() => { setMenuOn(true) }}><i className='bx bx-chevron-left'></i></p>
                     </div>
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
